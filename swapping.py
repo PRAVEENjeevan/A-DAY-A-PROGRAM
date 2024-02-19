2f48a91bc8101eefeff29eb9054b60877eeae5c2
PRAVEENjeevan <142286075+PRAVEENjeevan@users.noreply.github.com>
2024-02-19 21:45:42 +0530

    Add files via upload

---
+++ swapping.py
@@ -0,0 +1,17 @@
+x=int(input("enter the value of x:"))
+y=int(input("enter the value of y:"))
+
+x=x+y
+y=x-y
+x=x-y
+
+print(x,y)
+
+x=int(input("enter the value of x:"))
+y=int(input("enter the value of y:"))
+
+temp=x
+x=y
+y=temp
+
+print(x,y)
